82d295621e87b9faec7c695779bc4c2121027833
vampir <vampir@localhost.localdomain>
2022-11-06 22:27:33 +0300

    Init commit

---
+++ src/main/java/org/storageart/storageart/StorageArtApplication.java
@@ -0,0 +1,13 @@
+package org.storageart.storageart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageArtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageArtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/storageart/storageart/StorageArtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.storageart.storageart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageArtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
