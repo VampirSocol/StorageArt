8461ff63257497f4dc6e7f09b6c15389e0d5571f
VampirSocol <vampir@example.com>
2022-11-27 14:10:38 +0300

    Adding dto and fixing some bugs

--- src/main/java/org/storageart/storageart/service/UserService.java
+++ src/main/java/org/storageart/storageart/service/UserService.java
@@ -1,20 +1,32 @@
 package org.storageart.storageart.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.storageart.storageart.StorageArtApplication;
 import org.storageart.storageart.mapper.UserMapper;
 import org.storageart.storageart.repositories.UserRepository;
 
 @Service
 public class UserService implements UserDetailsService {
 
-    @Autowired
     private UserRepository userRepository;
 
     private UserMapper userMapper;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Autowired
+    public void setUserMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
